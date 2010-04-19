145770902dfe8cfdb16480059b3ac02cc3ad79a4
Johan Andersson <johan.rejeep@gmail.com>
2010-04-19 20:10:25 +0000

    Parentheses in wrong place.

--- test/keats-test.el
+++ test/keats-test.el
@@ -1,7 +1,7 @@
 (ert-deftest terminating-keys ()
   (should (terminating-key-p [13])) ;; RET
-  (should (terminating-key-p [7]))  ;; C-g)
+  (should (terminating-key-p [7]))) ;; C-g
 
 (ert-deftest non-terminating-keys ()
-  (should-not (terminating-key-p [16]))   ;; C-p
-  (should-not (terminating-key-p [3 13])) ;; C-c RET)
+  (should-not (terminating-key-p [16]))    ;; C-p
+  (should-not (terminating-key-p [3 13]))) ;; C-c RET
